6bd11d099723383da0dbda0ed20edb6d7cacf099
aka-sa <79243918+aka-sa@users.noreply.github.com>
2024-11-19 17:28:57 +0530

    Update detect.py

--- detect.py
+++ detect.py
@@ -1,30 +1,4 @@
-import streamlit as st
-import numpy as np
-import cv2
-from tensorflow.keras.models import load_model
-from PIL import Image
-from streamlit_drawable_canvas import st_canvas
-
-model = load_model('mnist.h5')
-
-st.title("Handwritten Digit Recognition Web App")
-
-st.write("Draw a digit below, and the model will predict the digit.")
-
-canvas_result = st_canvas(
-    fill_color="rgba(0, 0, 0, 0)", 
-    stroke_width=10,
-    stroke_color="#FFFFFF", 
-    background_color="#000000", 
-    height=200,
-    width=200,
-    drawing_mode="freedraw",
-    key="canvas"
-)
-
-if canvas_result.image_data is not None:
-    input_image = cv2.resize(canvas_result.image_data.astype('uint8'), (28, 28))
-    grayscale = cv2.cvtColor(input_image, cv2.COLOR_BGR2GRAY)
+vtColor(input_image, cv2.COLOR_BGR2GRAY)
     
     input_image_normalized = grayscale / 255.0
     
